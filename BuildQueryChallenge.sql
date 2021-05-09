ae9a8a7e312d859759da75bbe05384d4b2f4c84e
Stavros <101610510@student.swin.edu.au>
2021-05-10 09:53:51 +1000

    Task 1 Complete

--- BuildQueryChallenge.sql
+++ BuildQueryChallenge.sql
@@ -1,3 +1,20 @@
 --Stavros Kyriacou 101610510
 
 --USE BuildQueryChallengeOfficial;
+
+--RELATIONAL SCHEMA
+
+-- TOUR (TourName, Description)
+-- PRIMARY KEY (TourName)
+
+-- CLIENT (ClientID, Surname, GivenName, Gender)
+-- PRIMARY KEY (ClientID)
+
+-- EVENT (TourName, EventMonth, EventDay, EventYear, EventFee)
+-- PRIMARY KEY (EventYear, EventMonth, EventDay, TourName)
+-- FOREIGN KEY (TourName) REFERENCES Tour(TourName)
+
+-- BOOKING (ClientID, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked)
+-- PRIMARY KEY (ClientID, TourName, EventMonth, EventDay, EventYear)
+-- FOREIGN KEY (ClientID) REFERENCES Client(ClientID)
+-- FOREIGN KEY (TourName, EventYear, EventMonth, EventDay) REFERENCES Event(TourName, EventYear, EventMonth, EventDay)
