a5d83f9e40b1fe356ac479d494eb4ece52a999d8
Stavros <101610510@student.swin.edu.au>
2021-05-10 10:39:50 +1000

    Task 3 Complete

--- BuildQueryChallenge.sql
+++ BuildQueryChallenge.sql
@@ -90,19 +90,17 @@ INSERT INTO EVENT
 ('West',	'Jan',	    29,	        2016,	    225);
 
 INSERT INTO BOOKING
-(ClientID, TourName, EventMonth, EventDay, EventYear, Payment, DateBooked) VALUES
-(1,	'North',	'Jan',	9,	2016,	200,	'2015-12-10'),
-(2,	'North',	'Jan',	9,	2016,	200,	'2015-12-16'),
-(1,	'North',	'Feb',	13,	2016,	225,	'2016-01-08'),
-(2,	'North',	'Feb',	13,	2016,	125,	'2016-01-14'),
-(3,	'North',	'Feb',	13,	2016,	225,	'2016-02-03'),
-(1,	'South',	'Jan',	9,	2016,	200,	'2015-12-10'),
-(2,	'South',	'Jan',	16,	2016,	200,	'2015-12-18'),
-(3,	'South',	'Jan',	16,	2016,	200,	'2016-01-09'),
-(2,	'West',	    'Jan',	29,	2016,	225,	'2015-12-17'),
-(3,	'West',	    'Jan',	29,	2016,	200,	'2015-12-18');
-
-
+(ClientID,  TourName,   EventMonth, EventDay,   EventYear,  Payment,    DateBooked) VALUES
+(1,	        'North',	'Jan',	    9,	        2016,	    200,	    '2015-12-10'),
+(2,	        'North',	'Jan',  	9,	        2016,	    200,	    '2015-12-16'),
+(1,	        'North',	'Feb',  	13,	        2016,	    225,	    '2016-01-08'),
+(2,	        'North',	'Feb',	    13,	        2016,	    125,	    '2016-01-14'),
+(3,	        'North',	'Feb',	    13,	        2016,	    225,	    '2016-02-03'),
+(1,	        'South',	'Jan',	    9,	        2016,	    200,	    '2015-12-10'),
+(2,	        'South',	'Jan',	    16,	        2016,	    200,	    '2015-12-18'),
+(3,	        'South',	'Jan',	    16,	        2016,	    200,	    '2016-01-09'),
+(2,	        'West',	    'Jan',	    29,	        2016,	    225,	    '2015-12-17'),
+(3,	        'West',	    'Jan',	    29,	        2016,	    200,	    '2015-12-18');
 
 SELECT * FROM TOUR;
 SELECT * FROM CLIENT;
