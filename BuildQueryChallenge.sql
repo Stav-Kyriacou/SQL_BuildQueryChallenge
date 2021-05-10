2c1f7e7b52344d83cc4a4e226a8facdee7053405
Stavros <101610510@student.swin.edu.au>
2021-05-10 11:36:54 +1000

    Task 6 Complete

--- BuildQueryChallenge.sql
+++ BuildQueryChallenge.sql
@@ -70,17 +70,17 @@ GO
 
 INSERT INTO TOUR 
 (TourName,  Description) VALUES
-('North',	'Tour of wineries and outlets of the Bedigo and Castlemaine region'),
-('South',	'Tour of wineries and outlets of Mornington Penisula'),
+('North',   'Tour of wineries and outlets of the Bedigo and Castlemaine region'),
+('South',   'Tour of wineries and outlets of Mornington Penisula'),
 ('West',    'Tour of wineries and outlets of the Geelong and Otways region');
 
 
 INSERT INTO CLIENT
-(ClientID,  SurName,    GivenName,  Gender) VALUES
-(1,	        'Price',	'Taylor',	'M'),
-(2,	        'Gamble',	'Ellyse',	'F'),
-(3,	        'Tan',	    'Tilly',	'F'),
-(101610510, 'Kyriacou', 'Stavros',  'M');
+(ClientID,      SurName,    GivenName,  Gender) VALUES
+(1,             'Price',    'Taylor',   'M'),
+(2,             'Gamble',   'Ellyse',   'F'),
+(3,             'Tan',      'Tilly',    'F'),
+(101610510,     'Kyriacou', 'Stavros',  'M');
 
 INSERT INTO EVENT
 (TourName,  EventMonth, EventDay,   EventYear,  EventFee) VALUES
@@ -92,16 +92,16 @@ INSERT INTO EVENT
 
 INSERT INTO BOOKING
 (ClientID,  TourName,   EventMonth, EventDay,   EventYear,  Payment,    DateBooked) VALUES
-(1,	        'North',	'Jan',	    9,	        2016,	    200,	    '2015-12-10'),
-(2,	        'North',	'Jan',  	9,	        2016,	    200,	    '2015-12-16'),
-(1,	        'North',	'Feb',  	13,	        2016,	    225,	    '2016-01-08'),
-(2,	        'North',	'Feb',	    13,	        2016,	    125,	    '2016-01-14'),
-(3,	        'North',	'Feb',	    13,	        2016,	    225,	    '2016-02-03'),
-(1,	        'South',	'Jan',	    9,	        2016,	    200,	    '2015-12-10'),
-(2,	        'South',	'Jan',	    16,	        2016,	    200,	    '2015-12-18'),
-(3,	        'South',	'Jan',	    16,	        2016,	    200,	    '2016-01-09'),
-(2,	        'West',	    'Jan',	    29,	        2016,	    225,	    '2015-12-17'),
-(3,	        'West',	    'Jan',	    29,	        2016,	    200,	    '2015-12-18');
+(1,         'North',    'Jan',      9,          2016,       200,        '2015-12-10'),
+(2,         'North',    'Jan',      9,          2016,       200,        '2015-12-16'),
+(1,         'North',    'Feb',      13,         2016,       225,        '2016-01-08'),
+(2,         'North',    'Feb',      13,         2016,       125,        '2016-01-14'),
+(3,         'North',    'Feb',      13,         2016,       225,        '2016-02-03'),
+(1,         'South',    'Jan',      9,          2016,       200,        '2015-12-10'),
+(2,         'South',    'Jan',      16,         2016,       200,        '2015-12-18'),
+(3,         'South',    'Jan',      16,         2016,       200,        '2016-01-09'),
+(2,         'West',     'Jan',      29,         2016,       225,        '2015-12-17'),
+(3,         'West',     'Jan',      29,         2016,       200,        '2015-12-18');
 GO
 --Task 4
 --Query 1
